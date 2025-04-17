0776ed3b36d49935fd5f0fe1ff8e695161bb46b7
Hazim2024 <hazimiftikhar@gmail.com>
2025-04-17 15:42:06 +0100

    Removed Comment

--- app/login.php
+++ app/login.php
@@ -33,7 +33,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Login Page</title>
-  <link rel="stylesheet" href="../css/style.css">  <!-- adjust path as needed -->
+  <link rel="stylesheet" href="../css/style.css">  
 </head>
 <body>
   <div class="login-container">
